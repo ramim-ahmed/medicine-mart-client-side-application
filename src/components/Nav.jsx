a043fa1c9c77d3cdef38dfbad346792e990f45f4
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-31 23:40:25 +0600

    [DONE] navbar-section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/navLogo.png";
+import { RiMenuFoldLine } from "react-icons/ri";
 export default function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="py-3 border-b">
+      <div className="max-w-7xl px-3 mx-auto">
+        <div className="flex justify-between items-center">
+          <div>
+            <Link>
+              <img src={logo} alt="" />
+            </Link>
+          </div>
+          <div className="hidden lg:block">
+            <ul className="flex  items-center space-x-6">
+              <li>Home</li>
+              <li>Shop</li>
+              <li>Cart</li>
+              <li>Language</li>
+              <li>Join US</li>
+            </ul>
+          </div>
+          <div className="lg:hidden block">
+            <RiMenuFoldLine className="h-7 w-7" />
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
