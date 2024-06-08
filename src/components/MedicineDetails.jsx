00fcd30f58e6dfa62427eaf1bc24b2e5d0b2ef52
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-06-08 17:21:58 +0600

    [DONE] medicine-details-page

--- src/components/MedicineDetails.jsx
+++ src/components/MedicineDetails.jsx
@@ -1,5 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import PropTypes from "prop-types";
 import { Button } from "./ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useSecureApi from "@/hooks/useSecureApi";
+import toast from "react-hot-toast";
+import useAuth from "@/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 export default function MedicineDetails({ item }) {
   const {
     image,
@@ -11,6 +18,49 @@ export default function MedicineDetails({ item }) {
     company,
     category,
   } = item || {};
+  const secureApi = useSecureApi();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { authUser } = useAuth();
+  const { mutateAsync: addToCart } = useMutation({
+    mutationFn: async (data) => {
+      const res = await secureApi.post("/carts/create-new", data);
+      if (!res.data?.data) {
+        toast.error("Already Product Added To Cart!!");
+      } else {
+        toast.success("Product Added To Cart!!");
+      }
+    },
+    onSuccess: () => queryClient.invalidateQueries(["my-carts"]),
+  });
+
+  const handleAddToCart = async ({
+    _id,
+    id,
+    __v,
+    company,
+    category,
+    createdAt,
+    updatedAt,
+    ...res
+  } = product) => {
+    if (!authUser) {
+      return navigate("/login");
+    }
+    try {
+      const newData = {
+        email: authUser?.email,
+        productId: _id,
+        company: company?._id,
+        category: category?._id,
+        ...res,
+      };
+      await addToCart(newData);
+    } catch (error) {
+      toast.error("Product Add To Cart Failed!!");
+    }
+  };
+
   return (
     <div className="bg-gray-50">
       <div className="flex space-x-8 p-2">
@@ -26,7 +76,11 @@ export default function MedicineDetails({ item }) {
           <p>{company?.name}</p>
           <p>Unit Price : ${unitPrice}</p>
           <div>
-            <Button className="w-32" variant="outline">
+            <Button
+              onClick={() => handleAddToCart(item)}
+              className="w-32"
+              variant="outline"
+            >
               Select
             </Button>
           </div>
